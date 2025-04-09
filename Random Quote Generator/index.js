4b88fece80136e87b8ca6ea2816b6290ffb0348f
BarkingWaffle <BarkingWaffle27@gmail.com>
2025-04-09 17:31:24 -0500

    Wrote in quotes

---
+++ Random Quote Generator/index.js
@@ -0,0 +1,13 @@
+const quotes = [
+  “Travel changes you. As you move through this life and this world you change things slightly, you leave marks behind, however small. And in return, life—and travel—leaves marks on you.”,
+  “your body is not a temple, it's an amusement park. Enjoy the ride.”, “I don't have to agree with you to like you or respect you.”,
+  “Do we really want to travel in hermetically sealed popemobiles through the rural provinces of France, Mexico and the Far East, eating only in Hard Rock Cafes and McDonalds? Or do we want to eat without fear, tearing into the local stew, the humble taqueria's mystery meat, the sincerely offered gift of a lightly grilled fish head? I know what I want. I want it all. I want to try everything once.”,
+  “If you’re twenty-two, physically fit, hungry to learn and be better, I urge you to travel – as far and as widely as possible. Sleep on floors if you have to. Find out how other people live and eat and cook. Learn from them – wherever you go.”,
+  “Travel changes you. As you move through this life and this world you change things slightly, you leave marks behind, however small. And in return, life - and travel - leaves marks on you. Most of the time, those marks - on your body or on your heart - are beautiful. Often, though, they hurt.”,
+  “Maybe that’s enlightenment enough: to know that there is no final resting place of the mind; no moment of smug clarity. Perhaps wisdom...is realizing how small I am, and unwise, and how far I have yet to go.”,
+  “Once you’ve been to Cambodia, you’ll never stop wanting to beat Henry Kissinger to death with your bare hands. You will never again be able to open a newspaper and read about that treacherous, prevaricating, murderous scumbag sitting down for a nice chat with Charlie Rose or attending some black-tie affair for a new glossy magazine without choking. Witness what Henry did in Cambodia – the fruits of his genius for statesmanship – and you will never understand why he’s not sitting in the dock at The Hague next to Milošević.”,
+  “No one understands and appreciates the American Dream of hard work leading to material rewards better than a non-American.”,
+  “If I'm an advocate for anything, it's to move. As far as you can, as much as you can. Across the ocean, or simply across the river. The extent to which you can walk in someone else's shoes or at least eat their food, it's a plus for everybody. Open your mind, get up off the couch, move.”
+]
+
+const usedIndexes = new Set();
