bfe9b8d2c43819a074f09da3acd9de9b8b694ca7
BarkingWaffle <BarkingWaffle27@gmail.com>
2025-04-09 17:18:55 -0500

    Finished checker

--- Palindrome Checker/index.js
+++ Palindrome Checker/index.js
@@ -1,6 +1,19 @@
 const input = document.getElementById("input");
+const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
 
 function check() {
   const value = input.value;
-  alert(value);
+  const reverse = reverseString(value);
+  
+  if (value === reverse) {
+    alert("P A L I N D R O M E")
+  } else {
+    alert("N O T")
+  }
+
+  input.value = ""
 }
